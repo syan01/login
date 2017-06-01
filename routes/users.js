25f0604a8ef97a9af87067f5cb23c7236ce9b42d
Shangqi Yan <syan01@nyit.edu>
2017-06-01 13:57:15 -0400

    login page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const name = "ysq";
+const code = "12345";
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+  res.render('login');
+});
+
+router.post('/', function(req, res, next){
+	const uname = req.body.uname;
+	const psw = req.body.psw;
+	console.log('post login');
+	console.log(uname);
+	console.log(psw);
+	if(uname == name && psw == code){
+		res.redirect('/');
+	}
+	else{
+		res.status(401).send("wrong password");
+	}
+
+
 });
 
 module.exports = router;

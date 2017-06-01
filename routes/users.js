0fb9086aacac088f1d1a8157f706c4aa6b8a1857
Shangqi Yan <syan01@nyit.edu>
2017-06-01 14:42:58 -0400

    add databases

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-const name = "ysq";
-const code = "12345";
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'ysqmy12365',
+	database : 'login'
+});
+
+connection.connect();
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
@@ -15,13 +26,18 @@ router.post('/', function(req, res, next){
 	console.log('post login');
 	console.log(uname);
 	console.log(psw);
-	if(uname == name && psw == code){
-		res.redirect('/');
-	}
-	else{
-		res.status(401).send("wrong password");
-	}
-
+	var sql = "SELECT pwd FROM user WHERE uname = '" + uname + "';"
+	console.log(sql);
+	connection.query(sql, function (error, results, fields) {
+		if (error) throw error;
+		if(results.length == 0)  	{
+			res.status(401).send("Cannot found this username")
+		}else if(psw == results[0].pwd){
+			res.redirect('/');
+		}else{
+			res.status(401).send("wrong password");
+		}	
+	});
 
 });
 
